d8470c4e23082efd1ce3dc04b918284a78887320
Seohyun Kim <quixote075@gmail.com>
2024-11-28 04:16:17 -0800

    feat: join game model and entity

--- src/main.py
+++ src/main.py
@@ -1,23 +1,40 @@
 import player
 from player.strategy import HumanStrategy, ComputerStrategy
 
-class Game:
+class GameModel:
   def __init__(self):
     human_strategy = HumanStrategy()
     computer_strategy = ComputerStrategy()
     self.play = player.GameContext(human_strategy, computer_strategy)
+  
+  def update(self):
+    self.play.next_turn()
 
-  def run(self):
-    while self.play.count<31:
-      self.play.next_turn()
-      print('\tcurrent number is..', self.play.count)
-    print(f'Game over, {self.play.state.id} lose!\n')
+class GameView:
+  def display_game_state(self, game_state):
+    print('\tcurrent number is..', game_state.count)
+
+  def display_winner(self, winner):
+    print(f'Game over, {winner} lose!\n')
+
+class GameController:
+  def __init__(self, model, view):
+    self.model = model
+    self.view = view
+
+  def game_play(self):
+    while self.model.play.count<31:
+      self.model.update()
+      self.view.display_game_state(self.model.play)
+    self.view.display_winner(self.model.play.state.id)
     input('Press ENTER to exit the program')
-    return
 
-def main():
-  game = Game()
-  game.run()
+def vs_computer():
+  model = GameModel()
+  view = GameView()
+  controller = GameController(model, view)
+
+  controller.game_play()
 
 if __name__=='__main__':
-  main()
+  vs_computer()
--- src/player/strategy.py
+++ src/player/strategy.py
@@ -1,17 +1,18 @@
-import random
+import sys, pygame, random, time
 
 class ComputerStrategy:
   def __init__(self):
     self.index = 0
     self.numbers = [2, 6, 10, 14, 18, 22, 26, 30, 31]
   def choose_number(self, current_count):
+    time.sleep(1)
     if current_count>=self.numbers[self.index]:
       self.index += 1
     diff = self.numbers[self.index]-current_count
     if diff<=3:
       return diff
     return random.randint(1, 3)
-
+       
 class HumanStrategy:
   def choose_number(self, current_count):
     while True:
@@ -24,3 +25,41 @@ class HumanStrategy:
         print('[ERR] Please enter a number between 1 and 3.')
       except ValueError:
         print('[ERR] Invalid input. Please enter an integer.')
+
+class HumanStrategy4Player:
+  def __init__(self):
+    self.observers = []
+
+  def choose_number(self, current_count):
+    while True:
+      number = int(self.my_input())
+      if current_count+number>31:
+        return 31-current_count
+      if 1<=number<=3:
+        return number
+      print('[ERR] Please enter a number between 1 and 3.')
+      
+  def my_input(self) -> int:
+    self.number = 0
+    while True:
+      for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+          pygame.quit()
+          sys.exit()
+        if event.type==pygame.KEYDOWN:
+          if event.key==pygame.K_RETURN:
+            return int(self.number)
+          elif event.unicode.isdigit():
+            self.number = event.unicode
+            self.notify(self.number)
+
+  def attach(self, observer):
+    self.observers.append(observer)
+  
+  def detach(self, observer):
+    self.observers.remove(observer)
+  
+  def notify(self, message):
+    for observer in self.observers:
+      observer.update(message)
+      
